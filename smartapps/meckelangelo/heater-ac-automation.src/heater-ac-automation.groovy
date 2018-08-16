d4a3bbbc294a15574dadffde4b96d4d39727d7bf
meckelangelo <meckelangelo@gmail.com>
2018-08-16 15:31:46 -0400

    Updated Icon

--- smartapps/meckelangelo/heater-ac-automation.src/heater-ac-automation.groovy
+++ smartapps/meckelangelo/heater-ac-automation.src/heater-ac-automation.groovy
@@ -4,9 +4,7 @@ definition(
     author: "David Meck",
     description: "Automate a heater/AC unit that is plugged into a smart outlet, based on contact sensor and/or motion sensor.",
     category: "My Apps",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+    iconUrl: "https://raw.githubusercontent.com/meckelangelo/STHeaterACAutomation/master/HeatCool.png")
 
 
 preferences {    
